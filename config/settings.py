7057e40981a27787ce7453043af9e9f91cfa6324
dibyashakya <dibyashakya12@gmail.com>
2026-03-08 18:22:49 +0545

    commit

--- config/settings.py
+++ config/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$m2@&vlr^mvd)b+-7nch$1wzz-26o75m3m7n$*)o#0d(kcc@1j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
