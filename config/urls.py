3df816e0a14d22ca68ca71cb3ad917d304d83444
dibyashakya <dibyashakya12@gmail.com>
2026-03-10 19:39:57 +0545

    update

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 # project level urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/grocery/', include('grocceryitem.urls')),
+    path('', RedirectView.as_view(url='/api/grocery/', permanent=False)),  # ← add this
 ]
